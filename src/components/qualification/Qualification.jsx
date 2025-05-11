d444aa2b5d71d89c29dc05af88e0eb0add7d1bcc
Rachit776 <rachitkumar776@gmail.com>
2025-05-11 19:00:45 +0530

    Updated Qualification components

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -10,10 +10,11 @@ const Qualification = () => {
 
   return (
     <section className="qualification section">
-      <h2 className="section__title">Qualification</h2>
-      <span className="section__subtitle">My personal journey</span>
+
 
       <div className="qualification__container container">
+      <h2 className="section__title">Qualification</h2>
+      <span className="section__subtitle">My personal journey</span>
         <div className="qualification__tabs">
           <div
             className={
