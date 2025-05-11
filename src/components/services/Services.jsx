24f9d99336861c7eba78490647e54051d67f6cf1
Rachit776 <rachitkumar776@gmail.com>
2025-05-11 17:23:13 +0530

    Added Services

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import './services.css'
 const Services = () => {
+    const[toggleState, setToggleState] = useState(0);
+    const toggleTab = (index) => {
+        setToggleState(index);
+    }
+
   return (
     <section className="services section" id="services">
         <h2 className="section__title">Services</h2>
@@ -9,18 +14,17 @@ const Services = () => {
         <div className="services__container container grid">
             <div className="services__content">
                 <div>
-                    <i className="uil uil services__icon"></i>
-                    <h3 className="services__title">Web Development</h3>
-                    <span className="services__subtitle">I create responsive and user-friendly websites.</span>
-                </div>
-
-                <span className="services__button">View More
+                    <i className="uil uil-web-grid services__icon"></i>
+                    <h3 className="services__title">
+                        Web <br />Development</h3>
+                    </div>
+                <span className="services__button" onClick={() => toggleTab(1)}>View More
                     <i className="uil uil-arrow-right services__button-icon"></i>
                 </span>
 
-                <div className="services__modal">
+                <div className={toggleState === 1 ? "services__modal active-modal" : "services__modal"}>
                     <div className="services__modal-content">
-                        <i className="uil uil-times services__modal-close"></i>
+                        <i onClick={()=> toggleTab(0)} className="uil uil-times services__modal-close"></i>
                         <h3 className="services__modal-title">Web Development</h3>
                         <p className="services__modal-description">I create responsive and user-friendly websites using the latest technologies.</p>
                         <ul className="services__modal-services grid">
@@ -39,26 +43,79 @@ const Services = () => {
                                 <p className="services__modal-info">Point Number 3 to be moted</p>
                             </li>
 
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">Point Number 4 to be moted</p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">Point Number 5 to be moted</p>
+                            </li>
+                            
                         </ul>
                     </div>
                 </div>
             </div>
 
-            <div className="services__contfent">
+            <div className="services__content">
                 <div>
-                    <i className="uil uil services__icon"></i>
-                    <h3 className="services__title">Web Development</h3>
-                    <span className="services__subtitle">I create responsive and user-friendly websites.</span>
-                </div>
-
-                <span className="services__button">View More
+                    <i className="uil uil-arrow services__icon"></i>
+                    <h3 className="services__title">Frontend <br/ >Development</h3>
+                    </div>
+                <span className="services__button" onClick={() => toggleTab(2)}>View More
                     <i className="uil uil-arrow-right services__button-icon"></i>
                 </span>
 
-                <div className="services__modal">
+                <div className={toggleState === 2 ? "services__modal active-modal" : "services__modal"}>
                     <div className="services__modal-content">
-                        <i className="uil uil-times services__modal-close"></i>
-                        <h3 className="services__modal-title">Web Development</h3>
+                        <i onClick={()=> toggleTab(0)} className="uil uil-times services__modal-close"></i>
+                        <h3 className="services__modal-title">a Development</h3>
+                        <p className="services__modal-description">I create responsive and user-friendly websites using the latest technologies.</p>
+                        <ul className="services__modal-services grid">
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">I develop the user interface.</p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">a Page development</p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">Point Number 3 to be moted</p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">Point Number 4 to be moted</p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">Point Number 5 to be moted</p>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div className="services__content">
+                <div>
+                    <i className="uil uil-edit services__icon"></i>
+                    <h3 className="services__title">Software<br/ >Developer</h3>
+                    </div>
+                <span className="services__button" onClick={() => toggleTab(3)} >View More
+                    <i className="uil uil-arrow-right services__button-icon"></i>
+                </span>
+
+                <div className={toggleState === 3 ? "services__modal active-modal" : "services__modal"}>
+                    <div className="services__modal-content">
+                        <i onClick={()=> toggleTab(0)} className="uil uil-times services__modal-close"></i>
+                        <h3 className="services__modal-title">a Development</h3>
                         <p className="services__modal-description">I create responsive and user-friendly websites using the latest technologies.</p>
                         <ul className="services__modal-services grid">
                             <li className="services__modal-service">
@@ -76,11 +133,21 @@ const Services = () => {
                                 <p className="services__modal-info">Point Number 3 to be moted</p>
                             </li>
 
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">Point Number 4 to be moted</p>
+                            </li>
+
+                            <li className="services__modal-service">
+                                <i className="uil uil-check-circle services__modal-icon"></i>
+                                <p className="services__modal-info">Point Number 5 to be moted</p>
+                            </li>
+
                         </ul>
                     </div>
                 </div>
             </div>
-        </div>
+        </div>    
     </section>
   )
 }
