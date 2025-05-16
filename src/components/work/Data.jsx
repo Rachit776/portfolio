236a3f7c8ad4150289545962134fc2e40852cb9d
Rachit776 <rachitkumar776@gmail.com>
2025-05-16 14:13:27 +0530

    Added Projects

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,20 +1,28 @@
 import Work1 from "../../assets/work1.png";
 import Work2 from "../../assets/work2.png";
+import Work3 from "../../assets/work3.png";
 
 export const projectsData = [
     {
         id:1,
+        image: Work3,
+        title:'EtherLink',
+        category:'Web',
+        link: `https://ether-link.vercel.app/`,
+    },  
+    {
+        id:2,
         image: Work1,
         title:'Stimulus',
         category:'App',
         link: `https://github.com/Rachit776/Stimulus`,
     },
     {
-        id:2,
+        id:3,
         image: Work2,
         title:'Portfolio',
         category:'Web',
-        link: `https://github.com/Rachit776/portfolio`,
+        link: `portfolio-rachit776s-projects.vercel.app/`,
     },
 ];
 
