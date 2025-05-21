de6eac698a08758a92b1c6d7d701a98acacbbea4
Rachit776 <rachitkumar776@gmail.com>
2025-05-21 15:24:38 +0530

    Added Projects

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,14 +1,23 @@
 import Work1 from "../../assets/work1.png";
 import Work2 from "../../assets/work2.png";
 import Work3 from "../../assets/work3.png";
+import Work4 from "../../assets/work4.png";
 
 export const projectsData = [
+    
+    {
+        id:4,
+        image: Work4,
+        title:'AI-Interview-Simulator',
+        category:'Web',
+        link: `https://github.com/Rachit776/AI-Interview-Simulator`,
+    },
     {
         id:1,
         image: Work3,
         title:'EtherLink',
         category:'Web',
-        link: `https://ether-link.vercel.app/`,
+        link: `https://github.com/Rachit776/EtherLink`,
     },  
     {
         id:2,
@@ -24,6 +33,7 @@ export const projectsData = [
         category:'Web',
         link: `portfolio-rachit776s-projects.vercel.app/`,
     },
+    
 ];
 
 export const projectsNav = [
